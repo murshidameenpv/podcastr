d442be96fed53343c0ef3a97195e93c69c3ef750
murshidameen <murshidmonzpv@gmail.com>
2024-06-11 14:46:11 +0530

    basic routing setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,7 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -5,11 +9,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
-        <p className="text-white-1">lEFT SIDEBAR</p>
-        {children}
-        <p className="text-white-1">RIGHT SIDEBAR</p>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar/>
+        <section className="flex flex-col min-h-screen flex-1 px-4 sm:px-14">
+          <div className="mx-auto w-full flex max-w-5xl flex-col max-sm:px-4">
+            <div className="flex items-center justify-between h-16 md:hidden">
+              <Image src="/icons/logo.svg"  alt="menu open" width={30} height={30}/>
+              <MobileNav/>
+            </div>
+            <div className="flex flex-col md:pb-14">
+              Toaster
+              {children}
+            </div>
+          </div>
+        </section>
+        <RightSidebar/>
       </main>
     </div>
   );
---
+++ components/MobileNav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MobileNav = () => {
+  return (
+    <div className='text-white-1'>MobileNav</div>
+  )
+}
+
+export default MobileNav
