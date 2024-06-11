d60ccb39d26a27e1f63e8fed2407f1f92b80dc50
murshidameen <murshidmonzpv@gmail.com>
2024-06-11 17:13:36 +0530

    next configured for image url,displayed dummy podcast data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,25 @@
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
 
 const Home = () => {
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="podcast_grid">
+          {podcastData.map((podcast) => (
+            <PodcastCard
+              key={podcast.id}
+              imgUrl={podcast.imgURL}
+              title={podcast.title}
+              description={podcast.description}
+              podcastId={podcast.id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
